e083a2ff4b6b037fd22bfbcdd4093779c7795d21
jrodcab519 <jrodcab519@g.educaand.es>
2023-10-11 11:15:14 +0200

    versión inicial

--- src/Condicional5.java
+++ src/Condicional5.java
@@ -5,7 +5,14 @@ public class Condicional5 {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
+        System.out.println(" Introduce número de alumnos");
+        int alumno = s.nextInt();
 
-
+        if (alumno > 100) {
+            System.out.println("El costo es de 70 euros");
+        }
+        if (alumno >= 50 && alumno <= 99) {
+            System.out.println("El costo es de 95 euros");
+        }
     }
 }
