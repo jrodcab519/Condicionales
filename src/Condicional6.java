178c738f00aacffec169f0c1980fb2744d883a7a
jrodcab519 <jrodcab519@g.educaand.es>
2023-10-16 21:45:01 +0200

    versión inicial

--- src/Condicional6.java
+++ src/Condicional6.java
@@ -7,6 +7,10 @@ public class Condicional6 {
         Scanner s = new Scanner(System.in);
         System.out.println(" Introduce número de minutos");
         int minutos = s.nextInt();
+        System.out.println(" Introduce día: Lunes(L), Martes (M), Miércoles(X), Jueves(J), Viernes(V), Sábado(S) ó Domingo(D):");
+        char dia = s.next().charAt(0);
+        System.out.println(" Introduce turno: Turno de mañana(M) o Tarde(T)");
+        char turno = s.next().charAt(0);
 
         if (minutos <=5) {
             System.out.println("El costo es de " + minutos + " euros");
