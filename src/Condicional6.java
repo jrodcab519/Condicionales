6140453d1cce087bd0b3fa53d462b7e1bde471c6
jrodcab519 <jrodcab519@g.educaand.es>
2023-10-17 22:13:41 +0200

    versión inicial

--- src/Condicional6.java
+++ src/Condicional6.java
@@ -5,7 +5,7 @@ public class Condicional6 {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
-        System.out.println(" Introduce número de minutos");
+        System.out.println(" Introduce número de minutos: ");
         int minutos = s.nextInt();
         System.out.println(" Introduce día: Lunes(L), Martes (M), Miércoles(X), Jueves(J), Viernes(V), Sábado(S) ó Domingo(D):");
         char dia = s.next().charAt(0);
@@ -13,17 +13,26 @@ public class Condicional6 {
         System.out.println(" Introduce turno: Turno de mañana(M) o Tarde(T)");
         char turno = s.next().charAt(0);
 
-        if (minutos <=5) {
-            System.out.println("El costo es de " + minutos + " euros");
+        double costeMinutos = 0.0;
+        if (minutos <= 5) {
+            costeMinutos = 1.0;
+        } else if (minutos <= 8) {
+            costeMinutos = 1.0 + (minutos - 5) * 0.8;
+        } else if (minutos <= 10) {
+            costeMinutos = 1.0 + 3 * 0.8 + (minutos - 8) * 0.7;
+        } else {
+            costeMinutos = 1.0 + 3 * 0.8 + 2 * 0.7 + (minutos - 10) * 0.5;
         }
-        if (minutos >5 && minutos<=8) {
-            System.out.println("El costo es de " + (5 + 0.80*(minutos-5)) + " euros");
+
+        if (dia == 'D') {
+            costeMinutos += costeMinutos * 0.03;
+        } else if (turno == 'M') {
+            costeMinutos += costeMinutos * 0.15;
+        } else if (turno == 'T') {
+            costeMinutos += costeMinutos * 0.10;
         }
-        if (minutos >8 && minutos<= 10) {
-            System.out.println("El costo es de " + (7.4 + 0.70*(minutos-8)) + " euros");
-        }
-        if (minutos > 10) {
-            System.out.println("El costo es de " + (8.8 + 0.50*(minutos-10))+ " euros");
-        }
+
+        System.out.println("El coste de la llamada es de :" + costeMinutos );
+
     }
 }
